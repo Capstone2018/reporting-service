70f377774565d5c9ed73e533879967a043e026e7
aethanol <aethan@uw.edu>
2018-02-02 15:08:39 -0800

    Add dummy authentication for users who aren't signed in

--- server/handlers/authenticated.go
+++ server/handlers/authenticated.go
@@ -2,18 +2,33 @@ package handlers
 
 import (
 	"net/http"
+
+	"github.com/Capstone2018/reporting-service/server/sessions"
+	"github.com/aethanol/challenges-aethanol/apiserver/models/users"
 )
 
+// AuthenticatedHandler defines an interface of a handler that can take a session state as parameter
 type AuthenticatedHandler func(http.ResponseWriter, *http.Request, *SessionState)
 
+// Authenticated is a middleware handler that returns a HanderFunc
 func (ctx *Context) Authenticated(handlerFunc AuthenticatedHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		sessState := &SessionState{}
-		// _, err := sessions.GetState(r, ctx.SessionSigningKey, ctx.SessionStore, sessState)
-		// if err != nil {
-		// 	http.Error(w, err.Error(), http.StatusUnauthorized)
-		// 	return
-		// }
+		_, err := sessions.GetState(r, ctx.SessionSigningKey, ctx.SessionStore, sessState)
+		if err != nil {
+			// generate a dummy user for users who aren't authenticated
+			// only on the POST /reports
+			if r.Method == "POST" && r.URL.Path == "/reports" {
+				sessState.User = &users.User{
+					ID:    1,
+					Email: "test@snopes.com",
+				}
+			} else {
+				http.Error(w, err.Error(), http.StatusUnauthorized)
+				return
+			}
+
+		}
 
 		handlerFunc(w, r, sessState)
 	}
