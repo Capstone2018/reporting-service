9e4b12b805b4b184ccaead3e0e8dd7563a06e73e
aethanol <aethan@uw.edu>
2018-02-23 00:40:05 -0800

    moving back to sql, postgres this time

--- scripts/sql/schema.sql
+++ scripts/sql/schema.sql
@@ -1,20 +1,20 @@
 -- schema for reporting db
-create table users (
-    id int(10) not null auto_increment,
+-- create table users (
+--     id serial not null unique,
 
-    primary key (id)
-);
+--     primary key (id)
+-- );
 
 create table hostnames (
-    id int(10) not null auto_increment,
+    id serial not null unique,
     host varchar(2083) not null,
 
     primary key (id)
 );
 
 create table urls (
-    id int(10) not null auto_increment,
-    host_id int(10) not null,
+    id serial not null unique,
+    host_id integer not null,
     path varchar(2083) not null,
 
     foreign key (host_id) references hostnames(id),
@@ -22,96 +22,14 @@ create table urls (
     primary key (id)
 );
 
-create table images (
-    id int(10) not null auto_increment,
-    og_url int(10) not null,
-    url_id int(10) not null,
-    type varchar(7),
-    width int(5),
-    height int(5),
-
-    foreign key (og_url) references opengraph(id),
-    foreign key (url_id) references urls(id),
-
-    primary key (id)
-);
-
-create table audios (
-    id int(10) not null auto_increment,
-    og_url int(10) not null,
-    url_id int(10) not null,
-    type varchar(7),
-    width int(5),
-    height int(5),
-
-    foreign key (og_url) references opengraph(id),
-    foreign key (url_id) references urls(id),
-    
-    primary key (id)
-);
-
-create table videos (
-    id int(10) not null auto_increment,
-    og_url int(10) not null,
-    url_id int(10) not null,
-    type varchar(7),
-    width int(5),
-    height int(5),
-    
-    foreign key (og_url) references opengraph(id),
-    foreign key (url_id) references urls(id),
-    
-    primary key (id)
-);
-
-create table profiles (
-    id int(10) not null auto_increment,
-    url_id int(10) not null,
-    firstname varchar(100),
-    lastname varchar(100),
-    username varchar(100),
-    gender varchar(10),
-    foreign key (url_id) references urls(id),
-    
-    primary key (id)
-);
-
-create table articles (
-    id int(10) not null auto_increment,
-    url_id int(10) not null,
-    
-    published_time datetime,
-    modified_time datetime,
-    expiration_time datetime,
-    section varchar(100),
-    tags varchar(300),
-
-    foreign key (url_id) references urls(id),
-    foreign key (author_id) references authors(id),
-    
-    primary key (id)
-);
-
-create table books (
-    id int(10) not null auto_increment,
-    url_id int(10) not null,
-    author_id int(10),
-
-    isbn varchar(100),
-    release_date datetime,
-    tags varchar(300),
-    foreign key (url_id) references urls(id),
-    
-    primary key (id)
-);
-
 create table opengraph (
-    id int(10) not null auto_increment,
-    url_id int(10) not null,
+    id serial not null unique,
+    created_at timestamp not null,
 
-    article_id int(10),
-    book_id int(10),
-    profile_id int(10),
+    url_id integer not null,
+    article_id integer,
+    book_id integer,
+    profile_id integer,
     
     title varchar(40),
     type varchar(7), 
@@ -119,20 +37,37 @@ create table opengraph (
     determiner varchar(5), 
     locale varchar(20),
     locales_alternate varchar(100),
+    images jsonb,
+    audios jsonb,
+    videos jsonb,
+    profiles jsonb,
+    articles jsonb,
+    books jsonb,
 
     foreign key (url_id) references urls(id),
 
     primary key (id)
 );
 
+create table query (
+    id serial not null unique,
+);
+
+create table page (
+    id serial not null unique,
+    created_at timestamp not null, 
+
+    primary key (id)
+);
+
 
 create table reports (
-    id int(10) not null auto_increment,
-    user_id int(10) not null,
-    og_id int(10) not null,
+    id serial not null unique,
+    user_id integer not null,
+    og_id integer not null,
     report_type varchar(100) not null,
     user_description varchar(1024) not null,
-    created_at datetime not null,
+    created_at timestamp not null,
 
     foreign key (user_id) references users(id),
     foreign key (og_id) references opengraph(id),
